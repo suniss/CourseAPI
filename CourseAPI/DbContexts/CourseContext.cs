f4c3d81fdd29e697296a169cc0b626e85cdb0307
sunisakc <sunisa.kan@gmail.com>
2021-11-05 16:02:32 +0100

    Add DbContext

---
+++ CourseAPI/DbContexts/CourseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseAPI.DbContexts
+{
+    public class CourseContext : DbContext
+    {
+        public CourseContext(DbContextOptions<CourseContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
